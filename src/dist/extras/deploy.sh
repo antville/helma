890a80f5e3bdb240c5e941c36207f8ee0608ce80
Tobi Schäfer <interface@p3k.org>
2026-07-05 21:50:06 +0200

    Simplify saving of local changes (no extra branch)

--- src/dist/extras/deploy.sh
+++ src/dist/extras/deploy.sh
@@ -14,19 +14,9 @@ case "$SSH_ORIGINAL_COMMAND" in
     printf '%s\n' 'done.'
     ;;
 
-  save\ *)
-    set -- $SSH_ORIGINAL_COMMAND
-    shift
-    branch_name=local-changes
-    git switch "$branch_name" 2>/dev/null || git switch --create "$branch_name"
-    git add --all
-    git diff --cached --quiet || git commit --message "$*"
-    ;;
-
   commit\ *)
     set -- $SSH_ORIGINAL_COMMAND
     shift
-    git switch main
     git add --all
     git diff --cached --quiet || git commit --message "$*"
     ;;
